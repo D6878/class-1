ef434dab09056fb75f4c4cedb5bb722e58d62dfb
D6878 <158477559+D6878@users.noreply.github.com>
2024-02-22 14:55:05 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-let IRL = 0
-let IRR = 0
 function Hard_Right () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 51)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
@@ -12,9 +10,14 @@ function Hard_left () {
     basic.pause(100)
     maqueen.motorStop(maqueen.Motors.All)
 }
+function Obstacle () {
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+}
 function soft_left () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
 }
 function turn () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
@@ -22,21 +25,36 @@ function turn () {
 }
 function soft_right () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
 }
 function all_ahead () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
 }
+let left_turn = 0
+let IRR = 0
+let IRL = 0
+while (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
+    maqueen.motorStop(maqueen.Motors.All)
+}
 basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
+        Obstacle()
+    }
     IRL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     IRR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     if (IRL == 0 && IRL == 0) {
         all_ahead()
     } else if (IRL == 0) {
         soft_left()
+        left_turn = 1
     } else if (IRR == 0) {
-        Hard_Right()
+        soft_right()
+        left_turn = 0
     } else {
-        turn()
+        if (left_turn) {
+            soft_right()
+        } else {
+            soft_left()
+        }
     }
 })
